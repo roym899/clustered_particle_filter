a6fae57338b5f3147c8074e21a79d78e42453861
Jonas Hongisto <hongisto@kth.se>
2017-12-13 18:43:02 +0100

    Add rough outline to run_mcl_cpf

--- src/CPF/run_mcl_cpf.m
+++ src/CPF/run_mcl_cpf.m
@@ -1,16 +1,31 @@
+% function run_mcl_cpf(map,robot,data,mode)
+% Principal run function. main.
 
+% Inputs:
+%			map:			struct
+%			robot:          struct
+%			data:			struct
+%           mode:           1X1
 
 
 
 function run_mcl_cpf(map, robot, data,mode)
-
-
-
-
-
-
-
-
-
-
+t = 0;
+[S,R,Q,u] = init();
+%z = data...
+while 1 
+    t = t+1;
+    switch flag
+        case 1
+            if((t*mode)<10)
+                S = mcl(S,R,Q,z,u,t,map,robot);
+            else
+                C = cluster(S);
+                flag = 2;
+            end
+        case 2 
+            S = mclcluster(S,C);
+    end
+    
+end
 end
