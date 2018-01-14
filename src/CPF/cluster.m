bfd4bb7cc56e8429de48323df94b58b821e98636
Jonas Hongisto <hongisto@kth.se>
2018-01-14 20:26:05 +0100

    Format  cluster.m

--- src/CPF/cluster.m
+++ src/CPF/cluster.m
@@ -4,10 +4,12 @@
 %
 % Inputs:
 %           S                   MX3 current particle set
-%           iterations          1x1 number of iteration in meanshift       
+%           minEuclidian        1x1 maximal euclidian distance for neighbourhood   
+%           minTheta            1x1 maximal angle distance for neighbourhood
+%           minNeighbours       1x1 minim number of neighbours to be a core
 % Outputs:
 %           C                   cell array containing the partition of S into a number of sets based on clusters. 
-function C = cluster(S,minEuclidian,minTheta,minNeighbours)
+function C = cluster(S,maxEuclidian,maxTheta,minNeighbours)
 Cl = 0;
 C = {};
 points = zeros(size(S,1),1);
@@ -15,13 +17,12 @@ for p = 1:size(S,1)
     if ((points(p))) 
         continue;
     end
-    d = pdist2((S(p,[1,2])),S(:,[1,2]))
+    d = pdist2((S(p,[1,2])),S(:,[1,2]));
     ang = zeros(size(S,1),1);
     for v = 1:size(S,1)
        ang(v) = abs(angdiff(S(p,3),S(v,3)));  
     end
-    ang
-    indx = find(not(points)&((d<minEuclidian)&(d>0)&(ang'<minTheta))');
+    indx = find(not(points)&((d<maxEuclidian)&(d>0)&(ang'<maxTheta))');
     N = S(indx,:);
     i = 1;
     M = size(N,1);
@@ -29,20 +30,19 @@ for p = 1:size(S,1)
         continue;
     end
     Cl = Cl +1;
-    points(p) = Cl
+    points(p) = Cl;
     while(i<M+1)
         if(points(indx(i)))
             i=i+1;
             continue;
         end
         
-        points(indx(i)) = Cl
-        d2 = pdist2(N(i,[1,2]),S(:,[1,2]))
-        %d2 = (d2<minEuclidian);
+        points(indx(i)) = Cl;
+        d2 = pdist2(N(i,[1,2]),S(:,[1,2]));
         for v = 1:size(S,1)
             ang(v) = abs(angdiff(N(i,3),S(v,3)));  
         end
-        neigh = points(((d2<minEuclidian)&(ang'<minTheta)))==0;
+        neigh = points(((d2<maxEuclidian)&(ang'<maxTheta)))==0;
         if(size(neigh)>minNeighbours)
             N = unique([N;S(neigh,:)],'rows');
             indx = unique([indx find(neigh)]);
@@ -60,6 +60,6 @@ for n = 1:size(points)
     end
 end
 
-C
+
 end
 
