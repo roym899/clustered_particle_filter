cbc372e451ffee9cbf99cb05b6719392f209f87f
Jonas Hongisto <hongisto@kth.se>
2018-01-14 20:11:06 +0100

    Fix cluster.m

--- src/CPF/cluster.m
+++ src/CPF/cluster.m
@@ -1,5 +1,5 @@
 % This function should returns the set of particles as partitions based on
-% clusters. Currently this is done by a variation of the Lloy's Algorithm
+% clusters. Currently this is done by a variation of the Lloyd's Algorithm
 % for K-means clustering.
 %
 % Inputs:
@@ -7,31 +7,59 @@
 %           iterations          1x1 number of iteration in meanshift       
 % Outputs:
 %           C                   cell array containing the partition of S into a number of sets based on clusters. 
-function C = cluster(S,iterations)
- Sr = S; 
- Stemp = S;
-r = 0,1; % This parameter decides what the radius of the func is.
-for i = 1:iterations
-    for p = 1:size(Sr,1)
-    d = pdist2((Sr(p,:)),S);
-    Stemp = S(S(S((d<r)&(d>0))),:);
-    if(isempty(Stemp))
-        Stemp = Sr(p,:);
+function C = cluster(S,minEuclidian,minTheta,minNeighbours)
+Cl = 0;
+C = {};
+points = zeros(size(S,1),1);
+for p = 1:size(S,1)
+    if ((points(p))) 
+        continue;
     end
-    Sr(p,:) = sum(Stemp,1)/size(Stemp,1);
+    d = pdist2((S(p,[1,2])),S(:,[1,2]))
+    ang = zeros(size(S,1),1);
+    for v = 1:size(S,1)
+       ang(v) = abs(angdiff(S(p,3),S(v,3)));  
     end
-    Sr
-    Sr = unique(Sr,'rows','stable')
+    ang
+    indx = find(not(points)&((d<minEuclidian)&(d>0)&(ang'<minTheta))');
+    N = S(indx,:);
+    i = 1;
+    M = size(N,1);
+    if(M<minNeighbours) 
+        continue;
+    end
+    Cl = Cl +1;
+    points(p) = Cl
+    while(i<M+1)
+        if(points(indx(i)))
+            i=i+1;
+            continue;
+        end
+        
+        points(indx(i)) = Cl
+        d2 = pdist2(N(i,[1,2]),S(:,[1,2]))
+        %d2 = (d2<minEuclidian);
+        for v = 1:size(S,1)
+            ang(v) = abs(angdiff(N(i,3),S(v,3)));  
+        end
+        neigh = points(((d2<minEuclidian)&(ang'<minTheta)))==0;
+        if(size(neigh)>minNeighbours)
+            N = unique([N;S(neigh,:)],'rows');
+            indx = unique([indx find(neigh)]);
+            M = size(N,1);
+        end
+        i = i+1;
+    end
+    
+    C{end+1} = unique([N;S(p,:)],'rows');
+    N = [];
+end
+for n = 1:size(points)
+    if(not(points(n)))
+        C{end+1} = S(n,:);
+    end
 end
 
-dist = pdist2(S,Sr);
-[~,idx] = min(dist,[],2);
-
-clusters = size(unique(idx),1);
-C = cell(1,clusters);
-
-for i = 1:clusters
-    C{i} = S(idx == i,:);
+C
 end
 
-end
