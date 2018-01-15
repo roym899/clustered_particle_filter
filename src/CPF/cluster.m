33cd17ef8e6e2e05727582d57564014767f85254
roym899 <roym899@gmail.com>
2018-01-15 14:02:29 +0100

    Fix clustering and add clustered plotting

--- src/CPF/cluster.m
+++ src/CPF/cluster.m
@@ -13,16 +13,20 @@ function C = cluster(S,maxEuclidian,maxTheta,minNeighbours)
 Cl = 0;
 C = {};
 points = zeros(size(S,1),1);
+d_euclidean = pdist2((S(:,[1,2])),S(:,[1,2]));
+angles = S(:,3);
+d_angle = repmat(angles, 1, size(S,1))-transpose(repmat(angles, 1, size(S,1)));
+d_angle = mod(d_angle,pi);
 for p = 1:size(S,1)
     if ((points(p))) 
         continue;
     end
-    d = pdist2((S(p,[1,2])),S(:,[1,2]));
-    ang = zeros(size(S,1),1);
-    for v = 1:size(S,1)
-       ang(v) = abs(angdiff(S(p,3),S(v,3)));  
-    end
-    indx = find(not(points)&((d<maxEuclidian)&(d>0)&(ang'<maxTheta))');
+    
+    ang = d_angle(p,:);
+    
+    exclude_me = ones(1,size(S,1));
+    exclude_me(p) = 0;
+    indx = find(not(points)'&((d_euclidean(p,:)<maxEuclidian)&(exclude_me)&(ang'<maxTheta)'));
     N = S(indx,:);
     i = 1;
     M = size(N,1);
@@ -36,22 +40,18 @@ for p = 1:size(S,1)
             i=i+1;
             continue;
         end
-        
         points(indx(i)) = Cl;
-        d2 = pdist2(N(i,[1,2]),S(:,[1,2]));
-        for v = 1:size(S,1)
-            ang(v) = abs(angdiff(N(i,3),S(v,3)));  
-        end
-        neigh = points(((d2<maxEuclidian)&(ang'<maxTheta)))==0;
+        ang = d_angle(indx(i),:);
+        neigh = points(((d_euclidean(i,:)<maxEuclidian)&(ang<maxTheta)))==0;
         if(size(neigh)>minNeighbours)
-            N = unique([N;S(neigh,:)],'rows');
-            indx = unique([indx find(neigh)]);
+            N = [N;S(neigh,:)];
+            indx = [indx find(neigh)];
             M = size(N,1);
         end
         i = i+1;
     end
     
-    C{end+1} = unique([N;S(p,:)],'rows');
+    C{end+1} = [N;S(p,:)];
     N = [];
 end
 for n = 1:size(points)
@@ -61,5 +61,4 @@ for n = 1:size(points)
 end
 
 
-end
-
+end	
---
+++ src/plotting_functions/plot_clusters.m
@@ -0,0 +1,17 @@
+function [ ] = plot_clusters( clustered_particles, threed )
+
+if nargin < 2
+    threed = false;
+end
+
+hold on
+for i=1:length(clustered_particles)
+    if threed
+        scatter3(clustered_particles{i}(:,1), clustered_particles{i}(:,2), clustered_particles{i}(:,3));
+    else
+        scatter(clustered_particles{i}(:,1), clustered_particles{i}(:,2));
+    end
+end
+hold off
+end
+
