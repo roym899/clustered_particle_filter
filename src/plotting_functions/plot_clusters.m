5997e11c0201eb765cadaa71bfd690a696998850
roym899 <roym899@gmail.com>
2018-01-17 13:10:17 +0100

    Fix scatter plot

--- src/plotting_functions/plot_clusters.m
+++ src/plotting_functions/plot_clusters.m
@@ -7,10 +7,10 @@ end
 hold on
 for i=1:length(clustered_particles)
     if threed
-        scatter3(clustered_particles{i}(:,1), clustered_particles{i}(:,2), clustered_particles{i}(:,3), 10, '.');
+        scatter3(clustered_particles{i}(:,1), clustered_particles{i}(:,2), clustered_particles{i}(:,3), 20, '.');
         zlim([-pi pi]);
     else
-        scatter(clustered_particles{i}(:,1), clustered_particles{i}(:,2),10,'.');
+        scatter(clustered_particles{i}(:,1), clustered_particles{i}(:,2),20,'.');
     end
 end
 hold off
